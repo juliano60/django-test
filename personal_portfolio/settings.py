b1f7dd1923eee80107f0d4928cb4cc13e275b4e7
Jules Konan <juliano60@hotmail.co.uk>
2021-09-19 08:47:06 +0100

    Switch debug on

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q$ag+(xbf85jzau=w5ep+2srzz6*cc^ysyd^6)w*16^a@r=yq7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.eilinoya-events.net']
 
